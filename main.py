0fa4030ca0134f6fc5bcae59cec56242c5fe2314
Lara Wilde <laramariewilde@gmail.com>
2026-03-24 09:59:41 +0000

    TEST

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main_menu():
     print("2. Track a Study Session")
     print("3. Check previous study sessions")
     print("4. Check what needs studying next")
+    print("5. Help/FAQ")
     printed_once = False
     while True:
         try:
@@ -33,31 +34,51 @@ def main_menu():
             return user_choice
         # make it so it checks if number is between 1-3 etc
 
-
-def test_time_func():
-    start = input("Start counting? Y/N")
-    if start.lower() == "y":
-        #marks time stamp for the stat
+    
+def get_study_time():
+    print("Time a study session selected.")
+    # make it so its a list of subjets and they choose one
+    print("What is this study session for?")
+    # add input cvalidation to ensure the inputs not an empty string
+    subject_studied = input("Please input your subject you're studing for:")
+    print("\n Input accepted.")
+    begin = input("Would you like to begin? Y/N")
+    if begin.lower() == "y":
         start = time.time()
-        print("Timer started")
     else:
-        print("Timer not started.")
-    stop = input("Stop counting? Y/N")
-    if stop.lower() == "y":
-        # marks time stamp for the end
+        #fix this so it returns that the user quits
+        return None
+    finished_studying = input("Enter 'Y' when you are finished studying and wish to end the timer.")
+    # make it so the user has to enter "y" for the program to end
+    if finished_studying.lower() == "y":
         end = time.time()
-        time_elapsed = end - start
-        print(time_elapsed)
+        # gets the time elapsed in seconds
+    elapsed_time = end - start
+    converted_time = convert_time(elapsed_time)
 
-    # rounded_time = round(time_elapsed, 1)
-    rounded_time = 180
-    if rounded_time < 60:
-        print("Studied for less than one minute.")
-        study_time_minutes = 1
-    elif rounded_time > 60:
-        study_time_minutes = rounded_time / 60
-    
-    print(study_time_minutes)
-    
+    if converted_time == False:
+        print("Time elapsed was less than one minute, this will not be logged.")
+    else:
+        print(f"You spent {converted_time} studying {subject_studied}. Well done!")
+        print("This will be logged.")
 
-test_time_func()
+def convert_time(elapsed_time):
+    elapsed_time = round(elapsed_time, 1)
+    print(f"TEST TEST TEST {elapsed_time}")
+    #checks if the elapsed time is atleast one minute
+    if elapsed_time < 60:
+        more_than_one_min = False
+    elif elapsed_time >= 60:
+        more_than_one_min = True
+
+    #returns if its more than one minute
+    if more_than_one_min == False:
+        return False
+    elif more_than_one_min == True:
+        time_into_mins = elapsed_time / 60
+        print(f"TESTTESTTEST 2 {time_into_mins}")
+
+        return time_into_mins
+
+        
+get_study_time()
